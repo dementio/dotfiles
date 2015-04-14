13d3475c266246e544536ba49d12370f1ad4cdf5
dementio <notjimcarrey@gmail.com>
2015-04-13 23:46:42 -0700

    Create new_install.sh

---
+++ new_install.sh
@@ -0,0 +1,1 @@
+apt-get install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake colordiff colortail ccze && pip install glances && git clone https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh && update-command-not-found
