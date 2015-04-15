8474f1465a00fb41adbfafde2374e752fb1a13c0
dementio <notjimcarrey@gmail.com>
2015-04-14 20:11:43 -0700

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -1,1 +1,10 @@
-apt-get install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake colordiff colortail ccze flex byacc libncurses5-dev libncursesw5-dev && pip install glances && git clone https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh && git clone http://git.code.sf.net/p/unnethack/git unnethack && update-command-not-found
+apt-get install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake colordiff colortail ccze flex byacc libncurses5-dev libncursesw5-dev
+pip install glances
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+git clone http://git.code.sf.net/p/unnethack/git unnethack
+update-command-not-found
+cd unnethack
+./configure --enable-curses-graphics
+make
+cd
