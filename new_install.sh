c1a9caf7a78335c9c2ba5b2bd082363ad8c30771
dementio <notjimcarrey@gmail.com>
2015-04-14 21:40:32 -0700

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -1,6 +1,8 @@
+mv non-free /etc/apt/sources.list.d/
+apt-get update
 apt-get install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake \
 colordiff colortail ccze flex byacc libncurses5-dev libncursesw5-dev irssi irssi-dev irssi-scripts lshw pastebinit deborphan \
-p7zip-full ntpdate beep
+p7zip-full ntpdate beep linux-firmware-nonfree
 echo 'C:12345:wait:/usr/bin/tail -n30 -f /var/log/messages | /usr/bin/ccze > /dev/tty12' >> /etc/inittab
 pip install glances
 sh ~/.vim_runtime/install_awesome_vimrc.sh
