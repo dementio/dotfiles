097401432f16f1e4d7221626bd0570fc4e95e1c8
dementio <notjimcarrey@gmail.com>
2015-04-14 22:24:29 -0700

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -5,6 +5,7 @@ apt-get install -y vim axel git gpm command-not-found elinks build-essential cta
 colordiff colortail ccze flex byacc libncurses5-dev libncursesw5-dev irssi irssi-dev irssi-scripts lshw pastebinit deborphan \
 p7zip-full ntpdate beep firmware-linux-nonfree
 echo 'C:12345:wait:/usr/bin/tail -n30 -f /var/log/messages | /usr/bin/ccze > /dev/tty12' >> /etc/inittab
+echo 'C:12345:wait:/usr/local/bin/glances > /dev/tty11' >> /etc/inittab
 pip install glances
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 update-command-not-found
