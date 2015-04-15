f65f832433e34a389fa397b729e6b7e7f113345c
dementio <notjimcarrey@gmail.com>
2015-04-14 21:15:10 -0700

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -1,4 +1,7 @@
-apt-get install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake colordiff colortail ccze flex byacc libncurses5-dev libncursesw5-dev irssi irssi-dev irssi-scripts
+apt-get install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake \
+colordiff colortail ccze flex byacc libncurses5-dev libncursesw5-dev irssi irssi-dev irssi-scripts lshw pastebinit deborphan \
+p7zip-full ntpdate beep
+echo 'C:12345:wait:/usr/bin/tail -n30 -f /var/log/messages | /usr/bin/ccze > /dev/tty12' >> /etc/inittab
 pip install glances
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 update-command-not-found
@@ -6,3 +9,4 @@ cd unnethack
 ./configure --enable-curses-graphics
 make
 cd
+beep
