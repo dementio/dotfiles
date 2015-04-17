8b3d7edc0feb5760a97f1768384056a42ee1d386
dementio <notjimcarrey@gmail.com>
2015-04-16 22:29:13 -0700

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -1,20 +1,31 @@
-mv non-free.list /etc/apt/sources.list.d/
-apt-get update
-apt-get -y dist-upgrade
-apt-get -y install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake \
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+ln -fs ~/dotfiles/.bashrc ~/.bashrc
+ln -fs ~/dotfiles/.profile ~/.profile
+source ~/.profile
+ln -s ~/dotfiles/.irssi ~/.irssi
+ln -s ~/dotfiles/bin ~/bin
+ln -s ~/dotfiles/.fonts ~/.fonts
+fc-cache -fv
+sudo ln -s /etc/apt/sources.list.d/non-free.list ~/dotfiles/non-free.list
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install vim axel git gpm command-not-found elinks build-essential ctags python-pip python-dev colorgcc colormake \
 colordiff colortail ccze flex byacc libncurses5-dev libncursesw5-dev irssi irssi-dev irssi-scripts lshw pastebinit deborphan \
 p7zip-full ntpdate beep firmware-linux-nonfree screen setserial
-echo 'C:12345:wait:/usr/bin/tail -n30 -f /var/log/messages | /usr/bin/ccze > /dev/tty12' >> /etc/inittab
-echo 'C:12345:wait:/usr/local/bin/glances > /dev/tty11' >> /etc/inittab
-pip install glances
-#git clone https://github.com/amix/vimrc.git .vim_runtime
+sudo echo 'C:12345:wait:/usr/bin/tail -n30 -f /var/log/messages | /usr/bin/ccze > /dev/tty12' >> /etc/inittab
+sudo echo 'C:12345:wait:/usr/local/bin/glances > /dev/tty11' >> /etc/inittab
+sudo pip install glances
+ln -s ~/dotfiles/.vim_runtime ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-update-command-not-found
-#git clone http://git.code.sf.net/p/unnethack/git unnethack
-cd unnethack
+sudo update-command-not-found
+cd ~/dotfiles/unnethack
 ./configure --enable-curses-graphics
 make
-make install
-cd
-mv .nethackrc /usr/local/share/unnethack/unnethackrc.default
+sudo make install
+sudo ln -fs ~/dotfiles/.nethackrc /usr/local/share/unnethack/unnethackrc.default
 beep
+fi
